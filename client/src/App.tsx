9c42b606c5655a075b24d0d5e2fe90a4642c3c4d
Kumar Adhikari <kumarad556@gmail.com>
2022-02-09 22:18:54 +0545

    managed routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 import Layout from './layouts';
+import Articles from './pages/Articles';
+import Projects from './pages/Projects';
 
 function App() {
 	return (
 		<Layout>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/articles" element={<Articles />} />
+				<Route path="/projects" element={<Projects />} />
 				<Route path="*" element={<div>404</div>} />
 			</Routes>
 		</Layout>
